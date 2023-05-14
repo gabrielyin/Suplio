b4370d33f85ec67438b4bbe9229414f2bbedbaed
gabrielyin <2021gabrielyin@gmail.com>
2023-05-14 11:25:07 -0300

    admin home page

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,7 +3,7 @@
 export default function Button({ onClick, text, width }) {
     return (
         <button
-            className={`bg-jade-600 hover:bg-jade-700 transition text-white py-2 rounded-xl tracking-wide border border-transparent ${width}`}
+            className={`bg-jade-600 hover:bg-jade-700 transition text-white py-2 px-3 rounded-xl tracking-wide border border-transparent ${width}`}
             onClick={() => onClick}
         >
             {text}
