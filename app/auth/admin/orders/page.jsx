0284783b1ba8578e106f99d830c63c3536403f97
gabrielyin <2021gabrielyin@gmail.com>
2023-05-14 11:33:53 -0300

    admin dashboard input fixed

--- app/auth/admin/orders/page.jsx
+++ app/auth/admin/orders/page.jsx
@@ -11,7 +11,7 @@ export default function Orders() {
             </header>
             <Container>
                 <header className="flex justify-between">
-                    <label className="bg-charade-50 flex px-2 py-1.5 border rounded-lg w-1/3 items-center gap-1.5 inline">
+                    <label className="h-fit bg-charade-50 flex px-2 py-1.5 border rounded-lg w-1/3 items-center gap-1.5 inline">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-charade-300">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                         </svg>
