b275baf0f2ba1f73a52bf4e6548a6bd63aa74367
gabrielyin <2021gabrielyin@gmail.com>
2023-05-14 11:03:21 -0300

    orders page

--- components/Container.jsx
+++ components/Container.jsx
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
     return (
-        <div className="rounded-lg shadow-[0_4px_39px_rgba(0,0,0,0.08)] h-full p-6">
+        <div className="bg-white rounded-lg shadow-[0_4px_39px_rgba(0,0,0,0.08)] h-full p-6 flex flex-col gap-4">
             {children}
         </div>
     )
